1a95f45468f9fe02171b4ddbdd4506f34b6610af
Allan Viray <allang.v.dev@gmail.com>
2023-08-08 17:12:07 -0500

    Create test pages and set up Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import '../sass/main.css';
+import TableOfContents from './pages/TableOfContents';
+import PageOne from './pages/PageOne';
+import PageTwo from './pages/PageTwo';
+import PageThree from './pages/PageThree';
 
 const App = () => {
   return (
-    <div>
-      <h1>SQL Notebook</h1>
-    </div>
+    <Routes>
+      <Route path="table-of-contents" element={<TableOfContents />} />
+      <Route path="page-one" element={<PageOne />} />
+      <Route path="page-two" element={<PageTwo />} />
+      <Route path="page-three" element={<PageThree />} />
+    </Routes>
   );
 };
 
---
+++ src/components/pages/TableOfContents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TableOfContents = () => {
+  return (
+    <div>
+      <h1>Table of Contents</h1>
+    </div>
+  );
+};
+
+export default TableOfContents;
