fbf408d4b9ff118e804dc84b7a506d0037740379
Allan Viray <allang.v.dev@gmail.com>
2023-08-12 12:22:00 -0500

    Update CreateTable content and styles

--- src/components/pages/CreateTable.js
+++ src/components/pages/CreateTable.js
@@ -10,28 +10,100 @@ const CreateTable = () => {
           </div>
           <div className="createTable">
             <article>
-              <h3>TL;DR</h3>
-              <p>Create a new, empty table by using CREATE TABLE.</p>
+              <h3>CREATE TABLE In Use</h3>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde
-                possimus asperiores neque odio voluptas totam dicta fugit labore
-                optio fuga!
+                Create a new, empty table by using the keyword, CREATE TABLE.
               </p>
             </article>
             <article>
-              <h3>About CREATE TABLE</h3>
+              <h4>Example</h4>
+              <p>Running this query:</p>
+              <div className="queryContainer">
+                <div className="query">
+                  <span>CREATE TABLE cities (</span>
+                  <span>name VARCHAR(50),</span>
+                  <span>country VARCHAR(50),</span>
+                  <span>population INTEGER,</span>
+                  <span>area INTEGER,</span>
+                  <span>);</span>
+                </div>
+              </div>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
-                nemo earum debitis quas, voluptatum odit adipisci facilis saepe
-                odio reprehenderit?
+                Returns a table called, "cities", and will look something like:
               </p>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Molestiae veniam quo inventore necessitatibus earum beatae
-                tempore consequuntur a sequi dolore nobis, ipsam, quam nam
-                laborum? Voluptatum sit suscipit culpa non?
-              </p>
+              <div className="tableContainer">
+                <h4>Table: countries</h4>
+                <table>
+                  <tr>
+                    <th>name</th>
+                    <th>country</th>
+                    <th>population</th>
+                    <th>area</th>
+                  </tr>
+                </table>
+              </div>
             </article>
+            <section>
+              <h3>Dissecting the query</h3>
+              <article>
+                <h4>Quick Summary</h4>
+                <p>
+                  This query tells the database that we want to create a table
+                  called, cities, with a name, country, population, and area
+                  columns. The terms next to the column names specify the
+                  column's data type.
+                </p>
+              </article>
+              <article>
+                <h4>What's happening?</h4>
+                <p>
+                  CREATE TABLE is our keyword, and cities is our identifier.
+                  Line 1 of the query tells the database that we want to create
+                  a table called, "cities".
+                </p>
+                <p>Lines 2-4 specify the column names and their data types.</p>
+                <div className="listContainer">
+                  <ul>
+                    <li>
+                      <span>
+                        For example, the name column will have a value type of
+                        VARCHAR(50) (see below for explanation), and the
+                        population column will have a value type of INTEGER (a
+                        number)
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </article>
+              <article>
+                <h4>About VARCHAR(n)</h4>
+                <p>
+                  VARCHAR(n) represents a string value type, with a max length
+                  of n.
+                </p>
+                <div className="listContainer">
+                  <ul>
+                    <li>
+                      <span>
+                        TEXT can also be used instead of VARCHAR(n) to represent
+                        a string data type. However, TEXT is better used for
+                        storing larger amounts of textual data.
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+                <p>
+                  In the example query, we created a table that has two
+                  columns--name and country--and we specified that their values
+                  will be VARCHAR(50).
+                </p>
+                <p>
+                  In other words, we are saying that the values of the name and
+                  country columns will be strings, and the strings cannot exceed
+                  50 characters.
+                </p>
+              </article>
+            </section>
           </div>
         </div>
       </section>
