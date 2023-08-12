972c2f17c0da2329fefc2d39371b87e04a700562
Allan Viray <allang.v.dev@gmail.com>
2023-08-12 09:22:07 -0500

    Update About content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,24 +10,26 @@ const About = () => {
           </div>
           <div className="mainContentContainer">
             <p>
-              Welcome to SQL Notebook! I built this project to serve as a go-to
-              resource for SQL-related notes, reminders, and more.
+              Welcome to SQL Notebook! I built this site to serve as a go-to
+              resource for SQL-related notes, reminders, and *eventually* more.
             </p>
             <p>
-              It currently houses a number of static pages only, but I'm aiming
-              to add additional features which might include:
+              It houses a number of only static, informational pages at the
+              moment, but I'm aiming to add additional features which might
+              include:
             </p>
             <ul className="contentList">
               <li>
                 <span>
-                  Access to a hosted sample database in order to send queries
-                  and display their results on a page.
+                  A simple in-browser code editor and access to a hosted sample
+                  database, in order to send queries and display their results
+                  on a page.
                 </span>
               </li>
               <li>
                 <span>
                   Randomized practice problems to allow the user to experiment
-                  with a query.
+                  with a query and test their understanding.
                 </span>
               </li>
             </ul>
